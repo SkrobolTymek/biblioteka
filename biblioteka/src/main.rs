ce6d2415ed766c1d59f85a730276550be442f7fa
SkrobolTymek <tymoteusz.skrobol@gmail.com>
2025-03-24 13:15:06 +0100

    added basic choices and implemented the quitting function

--- biblioteka/src/main.rs
+++ biblioteka/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::{self, Read};
-use std::io;
 
 #[derive(Debug)]
 struct Book{
@@ -15,5 +14,50 @@ fn main(){
     // let mut contents = String::new();
     // file.expect("REASON").read_to_string(&mut contents);
     // println!("File Contents:\n{}", contents);
-    
+
+    loop{
+    println!("CHOOSE AN OPTION: ");
+    println!("1 - READ A BOOK");
+    println!("2 - ADD A BOOK");
+    println!("3 - DELETE A BOOK ");
+    println!("4 - quit \n");
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("There was an error in reading the choice");
+    let choice = choice.trim();
+    match choice{
+        "1"=>{
+            
+        }
+        "2"=>{
+
+        }
+        "3"=>{
+
+        }
+        "4"=>{
+            println!("Are you sure? y/n");
+            let mut quitchoice = String::new();
+            io::stdin().read_line(&mut quitchoice).expect("There was an error while reading the quit choice");
+            let quitchoice = quitchoice.trim();
+            match quitchoice{
+                "y"=>{
+                    println!("QUITTING....");
+                    break;
+                }
+                "n"=>{
+                    println!("okey-dokey");
+                }
+                _=>{
+                    println!("You chose a non existing option");
+                }
+            }
+        }
+        _=>{
+            println!("You chose a wrong option");
+        }
+    }
+}
+
+
 }
